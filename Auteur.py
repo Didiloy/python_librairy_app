58a5e504974cfe3a7e1622f63677e658a26276f5
Didiloy <didiloy2001@gmail.com>
2021-12-30 17:33:59 +0100

    search by author added

--- Auteur.py
+++ Auteur.py
@@ -1,5 +1,19 @@
 class Auteur:
-    def __init__(self, nom, prenom, dateDeNaissance):
+    def __init__(self, nom):
         self.nom = nom                         # str
-        self.prenom = prenom                   # str
-        self.dateDeNaissance = dateDeNaissance # datetime
+        self.dateDeNaissance = None # datetime
+
+    def getNom(self):
+        return self.nom
+
+    def setNom(self, nom):
+        self.nom = nom
+
+    def getDateDeNaissance(self):
+        return self.dateDeNaissance
+
+    def setDateDeNaissance(self, dateDeNaissance):
+        self.dateDeNaissance = dateDeNaissance
+
+    def toString(self):
+        print(f"Nom : {self.nom}\nDate de naissance : {self.dateDeNaissance}")
--- testRequeteJson.py
+++ testRequeteJson.py
@@ -1,38 +1,72 @@
 import requests
 import json
+from Auteur import Auteur
 from Livre import Livre
 
+def globalSearch(search):
+    # Making a get request
+    response = requests.get(f'https://openlibrary.org/search.json?q={search}&&mode=everything')
+    #print(response.text)
+    # print response
+    #print(response)
 
-search = input("Rentre un livre a rechercher: ")
-# Making a get request
-response = requests.get(f'https://openlibrary.org/search.json?q={search}&&mode=everything')
-#print(response.text)
-# print response
-#print(response)
+    # print json content
+    #print(response.json())
 
-# print json content
-#print(response.json())
+    fileToWrite = open('answer.json', 'w+') #Ecrire la reponse au format json dans un fichier json
+    fileToWrite.write(response.text)
+    fileToWrite.close()
+    data = json.loads(response.text) #Transformer le texte en objet json
+    i = 0
+    liste_livre = []
+    for books in data['docs']:
+        if i > 5:
+            break
+        if 'author_name' not in books or books['author_name'] == None or books['author_name'] == []:
+            #print('Livre: ' + str(books['title']) + ' et pas d\'auteur')
+            liste_livre.append(Livre(str(books['title'])))
+            print(liste_livre[i].toString())
+            i += 1
+        else:
+            #print('Livre: ' + str(books['title']) + ' par l\'auteur ' + str(books['author_name'][-1]) )
+            liste_livre.append(Livre(str(books['title'])))
+            liste_livre[i].setAuthor(str(books['author_name'][-1]))
+            print(liste_livre[i].toString())
+            i += 1
 
-fileToWrite = open('answer.json', 'w+') #Ecrire la reponse au format json dans un fichier json
-fileToWrite.write(response.text)
-fileToWrite.close()
 
-data = json.loads(response.text) #Transformer le texte en objet json
-
-#print(data)
-i = 0
-liste_livre = []
-for books in data['docs']:
-    if i > 5:
-        break
-    if 'author_name' not in books or books['author_name'] == None or books['author_name'] == []:
-        #print('Livre: ' + str(books['title']) + ' et pas d\'auteur')
-        liste_livre.append(Livre(str(books['title'])))
-        print(liste_livre[i].toString())
+def authorSearch(search):
+    response = requests.get(f'https://openlibrary.org/search/authors.json?q={search}&mode=everything')
+    fileToWrite = open('answer.json', 'w+') #Ecrire la reponse au format json dans un fichier json
+    fileToWrite.write(response.text)
+    fileToWrite.close()
+    data = json.loads(response.text) #Transformer le texte en objet json
+    i = 0
+    for datas in data['docs']:
+        if i >= 1:
+            break
+        if 'birth_date' not in datas:
+            auteur = Auteur(str(datas['name']))
+            auteur.toString()
+        else:
+            auteur = Auteur(str(datas['name']))
+            auteur.setDateDeNaissance(str(datas['birth_date']))
+        print(f"top work: {datas['top_work']}")
         i += 1
-    else:
-        #print('Livre: ' + str(books['title']) + ' par l\'auteur ' + str(books['author_name'][-1]) )
-        liste_livre.append(Livre(str(books['title'])))
-        liste_livre[i].setAuthor(str(books['author_name'][-1]))
-        print(liste_livre[i].toString())
-        i += 1
+
+def main():
+    print("que voulez vous rechercher ?")
+    print("-"*20)
+    print("1.livre\n2.auteur")
+    print("-"*20)
+    choix = input()
+    if choix == '1' :
+        print("que voulez vous rechercher ? ")
+        search = input()
+        globalSearch(search)
+    elif choix == '2':
+        print("que voulez vous rechercher ?")
+        search = input()
+        authorSearch(search)
+
+main()
